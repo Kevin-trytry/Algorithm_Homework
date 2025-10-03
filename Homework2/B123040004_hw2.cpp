0041707324b9c845b3dbacc8f57a4a56705006b0
Kevin-trytry <q940223kevin@gmail.com>
2025-10-03 21:14:51 +0800

    Greedy Algorithm implementation

--- Homework2/B123040004_hw2.cpp
+++ Homework2/B123040004_hw2.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 #include<cmath>
 #include<climits>
+#include<algorithm>
 using namespace std;
 
 class Point {
@@ -21,16 +22,51 @@ class Point {
 class TSP {
     public:
         TSP() {
-            MinDistance = 1000000;
+            MinDistance = INT_MAX;
         }
-        
         void Greedy(vector<Point> &cities, vector<Point> &visited, double distance, Point StartPoint) {
-        
+            // Link the last point to the start point
+            if (visited.size() == cities.size()) {
+                distance += Distance(visited.back(), StartPoint);
+                if (distance < MinDistance) {
+                    MinDistance = distance;
+                    MinDistanceResult = visited;
+                }
+                return;
+            }
+            // Greedy to find the nearest point
+            double two_point_mindistance = INT_MAX;
+            Point nearest_point;
+            for (auto &var : cities) {
+                if (Find(var, visited)) continue;
+                double newDistance = Distance(visited.back(), var);
+                if (newDistance < two_point_mindistance) {
+                    two_point_mindistance = newDistance;
+                    nearest_point = var;   
+                }
+            }
+            visited.push_back(nearest_point);
+            Greedy(cities, visited, distance + two_point_mindistance, StartPoint);
         }
+
+        /// @brief 
+        /// @param point 
+        /// @param visited 
+        /// @return if the point is in the visited vector
+        bool Find(Point point, vector<Point> &visited) {
+            for (auto &var : visited) {
+                if (var.PointNumber == point.PointNumber) return true;
+            }
+            return false;
+        }
+
+        double Distance(Point &a, Point &b) {
+            return sqrt(pow((a.X - b.X), 2) + pow((a.Y - b.Y), 2));
+        }
+
         double GetMinDistance() {
             return MinDistance;
         }   
-
         vector<Point> GetMinDistanceResult() {
             return MinDistanceResult;
         }
@@ -67,6 +103,12 @@ int main( ) {
 
     // implement tsp
     TSP tsp;
+    // try each point as the start point
+    for (auto &var : cities) {
+        vector<Point> visited;
+        visited.push_back(var);
+        tsp.Greedy(cities, visited, 0, var);
+    }
     double minDistance = tsp.GetMinDistance();
     vector<Point> minPermResult = tsp.GetMinDistanceResult();
     
